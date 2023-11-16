4a0f385cc1169dc66dfb9c17cde15bf97181686f
Cryxile <creeper.super99@gmai.com>
2023-11-16 18:24:04 +0300

    Created text formatting service

---
+++ src/main/java/com/example/demo/service/FormattingService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Joke;
+
+public interface FormattingService {
+    String beautifyJoke(Joke joke);
+}
---
+++ src/main/java/com/example/demo/service/impl/FormattingServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.Joke;
+import com.example.demo.service.FormattingService;
+import org.springframework.stereotype.Service;
+
+import static com.example.demo.utils.Constants.MESSAGE_FORMAT_TEMPLATE;
+
+@Service
+public class FormattingServiceImpl implements FormattingService {
+
+    @Override
+    public String beautifyJoke(Joke joke) {
+        return String.format(
+                MESSAGE_FORMAT_TEMPLATE,
+                joke.getId(),
+                joke.getType(),
+                joke.getOriginalLanguage(),
+                joke.getOriginalSetup(),
+                joke.getOriginalPunchline(),
+                joke.getTargetLanguage(),
+                joke.getTranslatedSetup(),
+                joke.getTranslatedPunchline()
+        );
+    }
+}
