e6fb746e410166fc7cef7f8992ed8c93bc23f18d
Cryxile <creeper.super99@gmai.com>
2023-11-16 20:41:14 +0300

    OriginalAndTranslatedJokeDTO.java.java renamed to JokeDTO.java Added Joke to JokeDTO change

--- src/main/java/com/example/demo/dto/JokeDTO.java
+++ src/main/java/com/example/demo/dto/JokeDTO.java
@@ -1,6 +1,6 @@
 package com.example.demo.dto;
 
-
+import com.example.demo.model.Joke;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +9,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class JokeDTO {
-    private String type;
-    private String setup;
-    private String punchline;
-    private Integer id;
+
+    public JokeDTO(Joke joke) {
+        id = joke.getId();
+        type = joke.getType();
+        originalSetup = joke.getOriginalSetup();
+        originalPunchline = joke.getOriginalPunchline();
+        translatedSetup = joke.getTranslatedSetup();
+        translatedPunchline = joke.getTranslatedPunchline();
+        originalLanguage = joke.getOriginalLanguage();
+        targetLanguage = joke.getTargetLanguage();
+    }
+    Integer id;
+    String type;
+    String originalSetup;
+    String originalPunchline;
+    String translatedSetup;
+    String translatedPunchline;
+    String originalLanguage;
+    String targetLanguage;
+
 }
