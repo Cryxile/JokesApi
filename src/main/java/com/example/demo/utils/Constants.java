b95dcbca2b3e55e132810c87ed2fbb341204548a
Cryxile <creeper.super99@gmai.com>
2023-11-16 17:55:41 +0300

    Added new constants

--- src/main/java/com/example/demo/utils/Constants.java
+++ src/main/java/com/example/demo/utils/Constants.java
@@ -2,11 +2,27 @@ package com.example.demo.utils;
 
 public class Constants {
 
-    //TODO public final static
-    public static final String RESOURCE_URL = "https://official-joke-api.appspot.com/random_joke";
-    public static final String TRANSLATION_URL = "https://deep-translate1.p.rapidapi.com/language/translate/v2";
-    public  static final String JOKES_FILE_PATH = "src/main/java/com/example/demo/db/jokes.json";
-    public  static final String META_FILE_PATH = "src/main/java/com/example/demo/db/jokes_meta.txt";
-    public static final String ORIGINAL_LANGUAGE = "en";
-    public  static final String TARGET_LANGUAGE = "ru";
+    public final static String RAPID_KEY = "X-RapidAPI-Key";
+    public final static String RAPID_HOST = "X-RapidAPI-Host";
+    public final static String ORIGINAL_LANGUAGE = "en";
+    public final static String FILE_CREATION_PATH = "src/main/java/com/example/demo/db/";
+    public final static String JOKES_FILE_PATH = "src/main/java/com/example/demo/db/jokes.json";
+    public final static String META_FILE_PATH = "src/main/java/com/example/demo/db/jokes_meta.txt";
+    public final static String FILE_ACCESS_MODE = "rw";
+    public final static String FILE_EXTENSION_TYPE = ".tmp";
+    public final static String MESSAGE_FORMAT_TEMPLATE = """
+                        Joke № %d
+                                        
+                        Topic: %s
+                                        
+                        %s version:
+                                        
+                        Joke: %s
+                        Punchline: %s
+                                        
+                        %s version:
+                                        
+                        Шутка: %s
+                        Разрывная: %s
+                        """;
 }
