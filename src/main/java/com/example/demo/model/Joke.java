e97df4196b0b3d98a7d28eec0f9c30995fc85bb5
Cryxile <creeper.super99@gmai.com>
2023-11-16 20:43:40 +0300

    OriginalAndTranslatedJoke.java renamed to Joke.java Added JokeDTO and JokeEntity to Joke  change

--- src/main/java/com/example/demo/model/Joke.java
+++ src/main/java/com/example/demo/model/Joke.java
@@ -1,6 +1,7 @@
 package com.example.demo.model;
 
-
+import com.example.demo.dto.JokeDTO;
+import com.example.demo.model.db.JokeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,41 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Joke {
-    private String type;
-    private String setup;
-    private String punchline;
+    public Joke(JokeEntity joke) {
+        id = joke.getId();
+        type = joke.getType();
+        originalSetup = joke.getOriginalSetup();
+        originalPunchline = joke.getOriginalPunchline();
+        translatedSetup = joke.getTranslatedSetup();
+        translatedPunchline = joke.getTranslatedPunchline();
+        originalLanguage = joke.getOriginalLanguage();
+        targetLanguage = joke.getTargetLanguage();
+    }
+    public Joke(JokeDTO joke) {
+        id = joke.getId();
+        type = joke.getType();
+        originalSetup = joke.getOriginalSetup();
+        originalPunchline = joke.getOriginalPunchline();
+        translatedSetup = joke.getTranslatedSetup();
+        translatedPunchline = joke.getTranslatedPunchline();
+        originalLanguage = joke.getOriginalLanguage();
+        targetLanguage = joke.getTargetLanguage();
+    }
     private Integer id;
+    private String type;
+    private String originalSetup;
+    private String originalPunchline;
+    private String translatedSetup;
+    private String translatedPunchline;
+    private String originalLanguage;
+    private String targetLanguage;
+
+
+    @Override
+    public String toString() {
+        return String.format("{\"id\":%d,\"type\":\"%s\",\"originalSetup\":\"%s\",\"originalPunchline\":\"%s\"," +
+                        "\"translatedSetup\":\"%s\",\"translatedPunchline\":\"%s\",\"originalLanguage\":\"%s\"," +
+                        "\"targetLanguage\":\"%s\"}" , id, type, originalSetup, originalPunchline, translatedSetup,
+                translatedPunchline, originalLanguage, targetLanguage);
+    }
 }
