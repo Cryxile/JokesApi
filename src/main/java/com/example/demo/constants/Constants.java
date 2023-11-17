00306068aa8f486904dc50bdc75509a2b2181c24
Cryxile <creeper.super99@gmai.com>
2023-11-17 03:23:05 +0300

    Deleted deprecated constants of FileOperationsService

---
+++ src/main/java/com/example/demo/constants/Constants.java
@@ -0,0 +1,23 @@
+package com.example.demo.constants;
+
+public class Constants {
+
+    public final static String RAPID_KEY = "X-RapidAPI-Key";
+    public final static String RAPID_HOST = "X-RapidAPI-Host";
+    public final static String ORIGINAL_LANGUAGE = "en";
+    public final static String MESSAGE_FORMAT_TEMPLATE = """
+            Joke № %d
+                            
+            Topic: %s
+                            
+            %s version:
+                            
+            Joke: %s
+            Punchline: %s
+                            
+            %s version:
+                            
+            Шутка: %s
+            Разрывная: %s
+            """;
+}
