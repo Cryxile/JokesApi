a58d867805d8829fbde28660ed858c248f7fe370
Cryxile <creeper.super99@gmai.com>
2023-11-17 03:32:25 +0300

    Renamed from JokeTranslationRq.java

--- src/main/java/com/example/demo/model/TranslationRq.java
+++ src/main/java/com/example/demo/model/TranslationRq.java
@@ -1,11 +1,11 @@
-package com.example.demo.dto;
+package com.example.demo.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class JokeTranslationRqDTO {
+public class TranslationRq {
     String q;
     String source;
     String target;
